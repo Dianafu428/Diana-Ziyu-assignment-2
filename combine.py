cef24b8113c44b2be27881fdd433943fa27ad2c9
zi-yu-lin <zi-yu-lin@users.noreply.github.com>
2020-10-19 02:33:13 +0800

    update

--- combine.py
+++ combine.py
@@ -208,5 +208,30 @@ text2 = download_book(url_Tom)
 score1 = SentimentIntensityAnalyzer().polarity_scores(text1)
 score2 = SentimentIntensityAnalyzer().polarity_scores(text2)
 
-print(score1)
-print(score2)
+# print(score1)
+# print(score2)
+
+def sentiment_analysis(d):
+    '''
+    Takes in an argument: d: a dictionary that contains pos, neg, neu, and compound scores 
+    Prints the sentiment analysis result
+    '''
+    print("Overall sentiment dictionary is : ", d) 
+    print("sentence was rated as ", d['neg']*100, "% Negative") 
+    print("sentence was rated as ", d['neu']*100, "% Neutral") 
+    print("sentence was rated as ", d['pos']*100, "% Positive") 
+
+    print("Sentence Overall Rated As", end = " ") 
+    
+    # decide overall sentiment as positive, negative and neutral by using the value of compound
+    if d['compound'] >= 0.05 : 
+        print("Positive") 
+    
+    elif d['compound'] <= - 0.05 : 
+        print("Negative") 
+    
+    else : 
+        print("Neutral")
+
+sentiment_analysis(score1)
+sentiment_analysis(score2)
